74d0315dfcad2dfea7a70752e7256669f2af947f
Josh Marinacci <276938+joshmarinacci@users.noreply.github.com>
2024-08-12 16:19:03 -0700

    more work

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
-import {Button, Checkbox, drawBlock, HBox, Label, RenderParameters, VBox} from "./test.ts";
-
-console.log("this is the beginning")
+import {
+    Button,
+    Checkbox,
+    drawBlock,
+    HBox,
+    Icon,
+    IconButton,
+    Label,
+    NumberInput,
+    RenderParameters,
+    SearchInput,
+    Separator,
+    TextInput,
+    ToggleButton,
+    VBox
+} from "./test.ts";
+import {Icons} from "./icons.ts";
 
 const canvas = document.createElement('canvas')
 
-canvas.width = 500
+canvas.width = 700
 canvas.height = 300
 
 document.body.appendChild(canvas)
@@ -14,16 +28,43 @@ const ctx = canvas.getContext('2d') as CanvasRenderingContext2D
 const c:RenderParameters = {
     ctx:ctx,
     fontSize:'26px',
-    debug:false
+    debug:true
 }
 c.ctx.font = '20px sans-serif'
-const vblock = HBox(c,[
-    Label(c,"label"),
-    Button(c,"Button"),
-    Checkbox(c,"Checkbox",true),
+
+
+const content = VBox(c, [
+    HBox(c,[
+        Label(c,'simple components'),
+        Button(c,"Button"),
+        ToggleButton(c,{text:"Toggle",selected:true}),
+        IconButton(c,{text:'Doc',icon:Icons.Document}),
+        // IconButton(c,{icon:Icons.Document, ghost:true}),
+        Icon(c,{icon:Icons.Document}),
+        // Tag(c, {text:'tag'}),
+        Checkbox(c,"Checkbox",true),
+    ]),
+    HBox(c, [
+        Label(c,'toolbar'),
+        HBox(c, [
+            Button(c,"Button"),
+            IconButton(c,{text:'IconButton',icon:Icons.Document}),
+            IconButton(c,{icon:Icons.Document}),
+            Separator(c),
+            IconButton(c,{icon:Icons.Document}),
+        ]),
+    ]),
+    HBox(c, [
+        Label(c,'inputs'),
+        Button(c,"Button"),
+        TextInput(c,{placeholder:'text'}),
+        NumberInput(c,{placeholder:'0'}),
+        SearchInput(c,{placeholder:'search'}),
+        Checkbox(c, 'check box', true),
+    ])
 ])
 
 ctx.save()
 ctx.translate(100,100);
-drawBlock(c,vblock)
+drawBlock(c,content)
 ctx.restore()
--- src/test.ts
+++ src/test.ts
@@ -1,81 +1,173 @@
 import {Bounds, Insets, Point} from "josh_js_util"
+import {Icons} from "./icons.ts";
 
 export type RenderParameters = {
-    ctx:CanvasRenderingContext2D
+    ctx: CanvasRenderingContext2D
     fontSize: string
-    debug:boolean
+    debug: boolean
 }
 
+
+const hue = 32
+const StyleVars = {
+    color: 'black',
+    borderColor: 'black',
+    fontSize: '16px',
+    fontFamily: 'sans-serif',
+    margin: new Insets(4, 4, 4, 4),
+    padding: 8,
+    background: `hsl(${hue},5%,100%)`,
+    selectedBackground: `hsl(${hue},90%,57%)`,
+    borderRadius: '4px',
+    borderWidth: '1px',
+    panelBackgroundColor: `hsl(${hue}, 5%, 100%)`,
+
+    controlBackgroundColor: `hsl(${hue}, 5%, 100%)`,
+    controlPadding: 8,
+    controlMargin: 4,
+    controlFontFamily: 'sans-serif',
+    controlFontSize: '16px',
+    controlTextColor: 'black',
+    controlBorderWidth: '1px',
+    controlBorderColor: `hsl(${hue}, 0%, 10%)`,
+
+    selectedControlBackground: `hsl(${hue}, 90%, 57%)`,
+}
 export type Border = {
-    width:number,
-    color:string
+    width: number,
+    color: string
 }
 
 
 export type VBlock = {
-    text?:string,
-    bounds:Bounds,
+    text?: string,
+    bounds: Bounds,
     margin?: Insets,
-    border?:Border,
+    border?: Border,
     padding?: Insets,
 
     icon?: "square-empty"
-    children?:VBlock[]
+    children?: VBlock[]
 
-    baseline:number,
-    background?:string
-    color?:string
+    baseline: number,
+    background?: string
+    color?: string
 }
 
-function addInsets(bounds: Bounds, padding: Insets):Bounds {
+function addInsets(bounds: Bounds, padding: Insets): Bounds {
     return new Bounds(bounds.x, bounds.y, bounds.w + padding.left + padding.right, bounds.h + padding.top + padding.bottom)
 }
 
-export function Button(c:RenderParameters, text:string):VBlock {
+export function Button(c: RenderParameters, text: string, bg?: string): VBlock {
     const m = c.ctx.measureText(text)
-    let bounds = new Bounds(0,0,m.width,m.fontBoundingBoxAscent+m.fontBoundingBoxDescent)
-    const padding = new Insets(5,5,5,5)
-    bounds = addInsets(bounds,padding)
-    return {text, bounds, children:[], baseline: m.fontBoundingBoxAscent,
-    background:"aqua", color:"white", padding: padding, border: {
-        width:1,
-        color:'black'
-    }}
+    let bounds = new Bounds(0, 0, m.width, m.fontBoundingBoxAscent + m.fontBoundingBoxDescent)
+    const padding = new Insets(5, 5, 5, 5)
+    bounds = addInsets(bounds, padding)
+    bounds = addInsets(bounds, StyleVars.margin)
+    return {
+        text,
+        bounds, children: [],
+        baseline: m.fontBoundingBoxAscent,
+        background: bg?bg:StyleVars.selectedBackground,
+        color: "black",
+        padding: padding,
+        border: {
+            width: 1,
+            color: 'black',
+        },
+        margin: StyleVars.margin
+    }
 }
 
 
 export function Checkbox(c: RenderParameters, text: string, b: boolean) {
     return HBox(c, [
         CheckIcon(c),
-        Label(c,'text')
+        Label(c, text)
     ])
 }
 
 
-export function CheckIcon(c:RenderParameters):VBlock {
+export function CheckIcon(c: RenderParameters): VBlock {
     return {
-        bounds: new Bounds(0,0,16,16),
+        bounds: new Bounds(0, 0, 16, 16),
         baseline: 8,
-        icon:"square-empty",
+        icon: "square-empty",
     }
 }
-export function Label(c:RenderParameters, text:string):VBlock {
+
+export function Label(c: RenderParameters, text: string): VBlock {
     const m = c.ctx.measureText(text)
-    let bounds = new Bounds(0,0,m.width,m.fontBoundingBoxAscent+m.fontBoundingBoxDescent)
-    const padding = new Insets(5,5,5,5)
-    bounds = addInsets(bounds,padding)
-    return {text: text, bounds: bounds, children:[], baseline: m.fontBoundingBoxAscent, padding:padding} as VBlock
-}
-export function VBox(_c: RenderParameters, vBlocks: VBlock[]):VBlock {
-    let bounds = new Bounds(0,0,0,0)
-    const border:Border = {
-        width:1,
-        color:'red',
+    let bounds = new Bounds(0, 0, m.width, m.fontBoundingBoxAscent + m.fontBoundingBoxDescent)
+    const padding = new Insets(5, 5, 5, 5)
+    bounds = addInsets(bounds, padding)
+    bounds = addInsets(bounds, StyleVars.margin)
+    return {
+        text: text,
+        bounds: bounds,
+        children: [],
+        baseline: m.fontBoundingBoxAscent,
+        padding: padding,
+        margin: StyleVars.margin,
     }
-    bounds.w += border.width*2
-    bounds.h += border.width*2
-    for(let ch of vBlocks) {
-        if(ch.bounds.w > bounds.w) {
+}
+
+export function ToggleButton(c: RenderParameters, opts: { text: string; selected: boolean }): VBlock {
+    if (opts.selected) {
+        return Button(c, opts.text)
+    } else {
+        return Button(c, opts.text)
+    }
+
+}
+
+
+type TextInputParameters = {
+    placeholder: string
+}
+
+export function TextInput(c: RenderParameters, opts: TextInputParameters): VBlock {
+    return HBox(c, [
+        Label(c, opts.placeholder)
+    ])
+}
+
+export function NumberInput(c: RenderParameters, opts: TextInputParameters) {
+    return TextInput(c, opts)
+}
+
+export function SearchInput(c: RenderParameters, opts: TextInputParameters) {
+    return TextInput(c, opts)
+}
+
+export function IconButton(c: RenderParameters, opts: { icon: string; text?: string }): VBlock {
+    const ch = [CheckIcon(c)]
+    if (opts.text) ch.push(Label(c, opts.text))
+    return HBox(c, ch)
+}
+
+export function Icon(c: RenderParameters, opts: { icon: Icons }): VBlock {
+    return CheckIcon(c)
+}
+
+export function Separator(c: RenderParameters): VBlock {
+    return {
+        bounds: new Bounds(0, 0, 10, 30),
+        background: "red",
+        baseline: 0
+    }
+}
+
+export function VBox(_c: RenderParameters, vBlocks: VBlock[]): VBlock {
+    let bounds = new Bounds(0, 0, 0, 0)
+    const border: Border = {
+        width: 1,
+        color: 'red',
+    }
+    bounds.w += border.width * 2
+    bounds.h += border.width * 2
+    for (let ch of vBlocks) {
+        if (ch.bounds.w > bounds.w) {
             bounds.w = ch.bounds.w
         }
         ch.bounds.y += bounds.h
@@ -83,23 +175,24 @@ export function VBox(_c: RenderParameters, vBlocks: VBlock[]):VBlock {
     }
     bounds = bounds.grow(1)
     return {
-        text:undefined,
-        bounds:bounds,
-        children:vBlocks,
+        text: undefined,
+        bounds: bounds,
+        children: vBlocks,
         baseline: 0,
-        border:border,
+        border: border,
     }
 }
-export function HBox(c:RenderParameters, children:VBlock[]):VBlock {
-    let bounds = new Bounds(0,0,0,0)
-    const border:Border = {
-        width:1,
-        color:'red',
+
+export function HBox(c: RenderParameters, children: VBlock[]): VBlock {
+    let bounds = new Bounds(0, 0, 0, 0)
+    const border: Border = {
+        width: 1,
+        color: 'red',
     }
-    bounds.w += border.width*2
-    bounds.h += border.width*2
-    let pt = new Point(bounds.x,bounds.y).add(new Point(2,2))
-    for(let ch of children) {
+    bounds.w += border.width * 2
+    bounds.h += border.width * 2
+    let pt = new Point(bounds.x, bounds.y).add(new Point(2, 2))
+    for (let ch of children) {
         ch.bounds.x = pt.x
         ch.bounds.y = pt.y
         pt.x += ch.bounds.w
@@ -108,48 +201,55 @@ export function HBox(c:RenderParameters, children:VBlock[]):VBlock {
     }
     bounds = bounds.grow(1)
     return {
-        text:undefined,
-        bounds:bounds,
-        children:children,
+        text: undefined,
+        bounds: bounds,
+        children: children,
         baseline: 0,
-        border:border,
+        border: border,
     }
 
 }
 
 export function drawBlock(c: RenderParameters, block: VBlock) {
     c.ctx.save()
-    if(c.debug) c.ctx.strokeRect(block.bounds.x,block.bounds.y,block.bounds.w,block.bounds.h)
-    c.ctx.translate(block.bounds.x, block.bounds.y)
-    if(block.background) {
-        c.ctx.fillStyle = block.background
-        c.ctx.fillRect(0,0,block.bounds.w,block.bounds.h)
+    if (c.debug) {
+        c.ctx.lineWidth = 0.5
+        c.ctx.strokeStyle = 'purple'
+        c.ctx.strokeRect(block.bounds.x, block.bounds.y, block.bounds.w, block.bounds.h)
     }
-    if(block.border) {
+    c.ctx.translate(block.bounds.x, block.bounds.y)
+    if (block.margin) {
+        c.ctx.translate(block.margin.left, block.margin.top)
+    }
+    if (block.background) {
+        c.ctx.fillStyle = block.background
+        c.ctx.fillRect(0, 0, block.bounds.w, block.bounds.h)
+    }
+    if (block.border) {
         c.ctx.strokeStyle = block.border.color
         c.ctx.lineWidth = block.border.width
-        c.ctx.strokeRect(0,0,block.bounds.w,block.bounds.h)
+        c.ctx.strokeRect(0, 0, block.bounds.w, block.bounds.h)
     }
-    if(block.padding) {
+    if (block.padding) {
         c.ctx.translate(block.padding.left, block.padding.top)
     }
     c.ctx.fillStyle = 'black'
-    if(block.color) {
+    if (block.color) {
         c.ctx.fillStyle = block.color
     }
     if (block.text) {
         c.ctx.fillText(block.text, 0, block.baseline)
     }
-    if(block.icon) {
-        if(block.icon === 'square-empty') {
+    if (block.icon) {
+        if (block.icon === 'square-empty') {
             c.ctx.strokeStyle = 'black'
             c.ctx.lineWidth = 1
-            c.ctx.strokeRect(0,0,16,16)
+            c.ctx.strokeRect(0, 0, 16, 16)
         }
     }
-    if(block.children) {
-        for(let ch of block.children) {
-            drawBlock(c,ch)
+    if (block.children) {
+        for (let ch of block.children) {
+            drawBlock(c, ch)
         }
     }
     c.ctx.restore()
