c282364781a966eef811fbcccf5c59a62d19b9fe
Josh Marinacci <276938+joshmarinacci@users.noreply.github.com>
2024-09-02 16:50:27 -0700

    more debug drawing

--- src/scene.ts
+++ src/scene.ts
@@ -3,11 +3,35 @@ import {
     GRenderNode,
     MKeyboardEvent,
     MMouseEvent,
-    MWheelEvent,
+    MWheelEvent, ZERO_INSETS,
 } from "./base.js";
 import {doDraw, drawDebug, fillRect, RenderContext, strokeBounds} from "./gfx.js";
-import {Bounds, Point, Size} from "josh_js_util";
+import {Bounds, Insets, Point, Size} from "josh_js_util";
 import {KEY_VENDOR} from "./keys.js";
+
+function text(ctx: CanvasRenderingContext2D, text: string, point: Point, valign, halign) {
+    ctx.textBaseline = valign
+    ctx.textAlign = halign
+    ctx.fillText(text,point.x,point.y)
+}
+
+function drawInsets(ctx: CanvasRenderingContext2D, b: Bounds, ins: Insets, color: string) {
+        fillRect(ctx, b, color)
+        ctx.fillStyle = 'black'
+        ctx.font = '11px sans-serif'
+        text(ctx, '' + ins.top, b.top_midpoint(), 'top', 'center')
+        text(ctx, '' + ins.bottom, b.bottom_midpoint(), 'bottom', 'center')
+        text(ctx, '' + ins.left, b.left_midpoint(), 'middle', 'left')
+        text(ctx, '' + ins.right, b.right_midpoint(), 'middle', 'right')
+}
+
+function isInvalid(size: Size) {
+    if(!size) return true
+    if(isNaN(size.w)) return true
+    if(isNaN(size.h)) return true
+    if(!(size instanceof Size)) return true
+    return false
+}
 
 export class Scene {
     private elementRoot!: GElement;
@@ -304,9 +328,9 @@ export class Scene {
         rc.ctx.fillStyle = fill
         rc.ctx.fillRect(bounds.x,bounds.y,bounds.w,bounds.h)
     }
-    private debugText(rc: RenderContext, bounds: Bounds, s: string) {
-        rc.ctx.fillStyle = 'black'
-        rc.ctx.fillText(s,bounds.x,bounds.y+20)
+    private debugText(rc: RenderContext, bounds: Bounds, text: string, color:string) {
+        rc.ctx.fillStyle = color || 'black'
+        rc.ctx.fillText(text,bounds.x,bounds.y+20)
     }
 
 
@@ -336,22 +360,44 @@ export class Scene {
 
     private drawDebugCompInfo(rc: RenderContext, comp: GRenderNode, w: number) {
         const lh = 16
-        let bounds = new Bounds(0,0,w,lh*5)
+        let bounds = new Bounds(0,0,w,100)
         fillRect(rc.ctx,bounds,'white')
         strokeBounds(rc,bounds,'black')
         let t = comp.settings
-        this.debugText(rc,bounds,` kind = ${t.kind}  key=${t.key}`)
+
+        rc.ctx.fillStyle = 'black'
+        rc.ctx.font = '11px sans-serif'
+        rc.ctx.textAlign = 'start'
+        rc.ctx.textBaseline = 'middle'
+
+        rc.ctx.save()
+        let b = new Bounds( w-100,0,100,100)
+        //fill rect around size
+        drawInsets(rc.ctx,b,comp.settings.borderWidth || ZERO_INSETS,'#aaaaaa')
+        b =b.grow(-15)
+        drawInsets(rc.ctx,b,comp.settings.padding || ZERO_INSETS,'#f0f0f0')
+        b =b.grow(-15)
+        fillRect(rc.ctx,b,'#f0d000')
+        //fill rect around inner content
+        rc.ctx.restore()
+
+        this.debugText(rc,bounds,` kind = ${t.kind}  key=${t.key}`,'black')
         rc.ctx.translate(0,lh)
-        this.debugText(rc,bounds,` pos = ${t.pos} size = ${t.size}`)
+        if(isInvalid(t.size)) {
+            this.debugText(rc,bounds,` pos = ${t.pos} size = ${t.size}`,'red')
+        } else {
+            this.debugText(rc,bounds,` pos = ${t.pos} size = ${t.size}`,'black')
+        }
         rc.ctx.translate(0,lh)
-        this.debugText(rc,bounds,` border = ${t.borderWidth}`)
-        rc.ctx.translate(0,lh)
-        this.debugText(rc,bounds,` padding = ${t.padding}`)
-        rc.ctx.translate(0,lh*3)
+        this.debugText(rc,bounds,` text = ${t.text}`)
+        // rc.ctx.translate(0,lh)
+        // this.debugText(rc,bounds,` padding = ${t.padding}`)
+
+
         comp.settings.children.forEach((ch,i) => {
             rc.ctx.save()
-            rc.ctx.translate(10,lh*6*i)
-            this.drawDebugCompInfo(rc,ch,w-15)
+            rc.ctx.translate(0,105*i+100)
+            this.drawDebugCompInfo(rc,ch,w)
             rc.ctx.restore()
         })
     }
